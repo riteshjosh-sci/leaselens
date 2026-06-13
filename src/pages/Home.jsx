f01e54c3fe873bf37210317a6bd27e27524890db
Riteshjosh <riteshjjoshi@gmail.com>
2026-06-13 10:38:11 +0530

    fix: add useState to React import in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import Footer from '../components/Footer'
